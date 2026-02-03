f38bac19939a972df5ab0b46f64fee897c50c2eb
yuva <yuva@gmail.com>
2026-02-03 07:56:54 +0530

    just commit

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -69,7 +69,7 @@ export default function Login() {
               type="password"
               value={password}
               onChange={(e) => setPassword(e.target.value)}
-              placeholder="••••••••"
+              placeholder=""
               autoComplete="current-password"
               disabled={loading}
             />
